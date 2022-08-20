eeb684e3403615fb0f100ddb0dd8b8a81ce82133
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-08-20 17:04:01 -0400

    feat:requirement10 complete thanks for a fun break 😉

--- services/productsService.js
+++ services/productsService.js
@@ -24,6 +24,10 @@ const register = async (name, id) => {
 const updateById = async (id, name) => {
   const [product] = await productsModel.getById(id);
   if (!product) throw new CustomerError(404, 'Product not found');
+  if (name === undefined) throw new CustomerError(400, '"name" is required');
+  if (name.length < 5) {
+    throw new CustomerError(422, '"name" length must be at least 5 characters long');
+  }
   const updatedProduct = await productsModel.updateById(id, name);
   return updatedProduct;
 };
